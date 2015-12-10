53e7f207fb9fb5798e3bf74d77882bc016041d6f
Greg Wiley <gwiley@cj.com>
2015-12-09 16:51:38 -0800

    new messages

--- src/docker/task-30/task.sh
+++ src/docker/task-30/task.sh
@@ -9,6 +9,6 @@ decrypt-file /twilio.config.encrypted /tmp/twcfg
 rm /tmp/twcfg
 rm /twilio.config.encrypted
 
-send-message "$dest" "starting task"
+send-message "$dest" "we have started your work"
 sleep 30
-send-message "$dest" "you have been served"
+send-message "$dest" "the missles have been launched"
