4ad588d2816d09b4247fa1cb01126c5e5e565ca5
Greg Wiley <gwiley@cj.com>
2015-12-09 16:33:00 -0800

    [config] simple build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+here=$(dirname $(readlink -f $0))
+
+pushd $here/src/docker/httpd
+docker build -t aztecrex/cj-httpd .
+popd
+
+pushd $here/src/docker/task-30
+docker build -t aztecrex/cj-task-30 .
+popd
